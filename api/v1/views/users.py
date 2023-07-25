5bb752864203b7296b34d4f888c54bd9dd447852
Stephaine Biggs <78326908+Sbiggs1985@users.noreply.github.com>
2023-07-25 12:21:07 -0500

    Update users.py

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,25 +1,18 @@
 #!/usr/bin/python3
-"""
-route for handling User objects and operations
-"""
+"""route for handling User objects and operations"""
 from flask import jsonify, abort, request
 from api.v1.views import app_views, storage
 from models.user import User
 
-
 @app_views.route("/users", methods=["GET"], strict_slashes=False)
 def user_get_all():
-    """
-    retrieves all User objects
-    :return: json of all users
-    """
+    """retirevies all User objects"""
     user_list = []
     user_obj = storage.all("User")
     for obj in user_obj.values():
         user_list.append(obj.to_json())
-
+    
     return jsonify(user_list)
-
 
 @app_views.route("/users", methods=["POST"], strict_slashes=False)
 def user_create():
